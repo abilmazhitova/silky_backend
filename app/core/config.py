15818441cad1b84bdaf16515e82ff00fbb0f6fc7
abilmazhitova <a_abilmazhitova@mail.ru>
2025-11-27 11:01:22 +0500

    add BACKEND_URL settings

--- app/core/config.py
+++ app/core/config.py
@@ -3,5 +3,5 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     API_1688_URL: str = "https://tstbnd.silkyway.kz/api"
     DATABASE_URL: str = "postgresql+asyncpg://postgres:test1234@localhost/silky"
-
+    BACKEND_URL: str = ""
 settings = Settings()
